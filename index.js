1b5d9e7aad7a70da4545530a36712974fc08c8d7
shivanshanand <shivanshanand962@gmail.com>
2025-03-19 09:42:55 +0530

    done

---
+++ index.js
@@ -0,0 +1,37 @@
+import express from "express";
+
+const app = express();
+
+const users = [
+  { name: "alice", age: 25, email: "alice@example.com" },
+  { name: "bob", age: 30, email: "bob@example.com" },
+  { name: "charlie", age: 28, email: "charlie@example.com" },
+];
+
+app.get("/user", (req, res) => {
+  try {
+    const name = req.query.name;
+
+    if (name === "") {
+      return res.status(403).send("User parameter cannot be empty");
+    }
+    const user = users.find((user) => user.name === name);
+
+    if (user) {
+      return res.status(200).json({
+        message: "User found",
+        data: { name: user.name, age: user.age, email: user.email },
+      });
+    } else {
+      return res.status(403).json({ message: "User not found" });
+    }
+  } catch (error) {
+    console.log("error", error);
+    return res.status(503).json({ error: error });
+  }
+});
+
+const port = 5000;
+app.listen(port, () => {
+  console.log("server running on port", port);
+});
